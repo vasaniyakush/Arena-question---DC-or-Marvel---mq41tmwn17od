3cdb8a7b1d6b955e92a6f369aa85e2db7ddf4482
Kush Vasaniya <vasaniyakush@gmail.com>
2023-07-28 00:29:58 +0530

    Initial commit

---
+++ app/Summary.js
@@ -0,0 +1,17 @@
+'use client'
+import { useState } from "react";
+function Summary({ formData }) {
+    return (
+        <div id="summary">
+            <h2>Summary</h2>
+            <p>Form Type: </p>
+            <p>Age: </p>
+
+            <p>DC Shows: </p>
+
+            <p>Marvel Shows: </p>
+
+        </div>
+    );
+}
+export default Summary;
