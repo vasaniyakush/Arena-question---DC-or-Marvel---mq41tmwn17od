53ec3754d50d630b4249ca1d20a9c4d9c0d40d82
vasaniyakush <vasaniyakush@gmail.com>
2023-07-28 01:55:28 +0530

    sol

--- app/Summary.js
+++ app/Summary.js
@@ -4,7 +4,7 @@ function Summary({ formData }) {
     return (
         <div id="summary">
             <h2>Summary</h2>
-            <p>Form Type: {formData.formType}</p>
+            <p>Form Type: Form {formData.formType}</p>
             <p>Age: {formData.age}</p>
             {formData.formType === "A" ? <p>DC Shows: {formData.Shows}</p> : <p>Marvel Shows: {formData.Shows}</p>}
         </div>
